e06aa5f615b35ccce4e115d8a1f91f3c59ad8f79
[fisherpine] <[3158669499@qq.com]>
2023-06-08 21:00:08 +0800

    项目搭建，启动类上加@MapperScan注解，编写实体类User，编写UserMapper,继承BaseMapper<?>

---
+++ src/main/java/com/ggbz/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.ggbz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ggbz.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/ggbz/pojo/User.java
@@ -0,0 +1,11 @@
+package com.ggbz.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
